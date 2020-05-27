dfef555cb9a5c468d21193f60080f212c9a77370
Turbo <turboyang@vip.qq.com>
2020-05-27 17:01:34 +0800

    Add example

---
+++ VsLinqIssue/VsLinqIssue/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VsLinqIssue
+{
+    class Program
+    {
+        public class Book
+        {
+            public int Id { get; set; }
+
+            public int OwnerId { get; set; }
+
+            public string Name { get; set; }
+        }
+
+        public class Person
+        {
+            public int Id { get; set; }
+
+            public string Nickname { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            var books = new List<Book>
+            {
+                new Book
+                {
+                    Id = 1,
+                    OwnerId = 1,
+                    Name = "Book1"
+                },
+                new Book
+                {
+                    Id = 2,
+                    OwnerId = 1,
+                    Name = "Book2"
+                }
+            };
+
+            var persons = new List<Person>
+            {
+                new Person
+                {
+                    Id = 1,
+                    Nickname = "Person1"
+                }
+            };
+
+            var join = persons.Join(books, person => person.Id, book => book.OwnerId/*There is no intellisense*/, (person, book) => new
+            {
+                person.Id,
+                person.Nickname,
+                book.Name/*There is no intellisense*/
+            });
+
+            var groupJoin = persons.GroupJoin(books, person => person.Id, book => book.OwnerId/*There is no intellisense*/, (person, books) => new
+             {
+                person.Id,
+                Name = books.Select(s=>s.Name)
+             });
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
